c11a5e24fc206cd498ae57b7257f208559d10fc1
Akash A <akashaakasha290@gmail.com>
2026-01-15 16:53:50 +0530

    api.js fix

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,11 @@
-import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const fetchUsers = () =>
-  axios.get("http://localhost:5000/api/users");
+export const loginUser = async (data) => {
+  const res = await fetch(`${BASE_URL}/api/users/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
+};
